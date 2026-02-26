ad552842b310a7438fa386f7f72447f436b955e4
amaterasutears <kirillufo@gmail.com>
2026-02-26 21:26:25 +0300

    feat: add extract username from update

--- extractor.go
+++ extractor.go
@@ -91,3 +91,48 @@ func ExtractChatIDFromUpdate(upd *models.Update) (int64, bool) {
 
 	return 0, false
 }
+
+func ExtractUsernameFromUpdate(upd *models.Update) (string, bool) {
+	if upd != nil {
+		switch {
+		case upd.Message != nil && upd.Message.From != nil:
+			return upd.Message.From.Username, true
+		case upd.EditedMessage != nil && upd.EditedMessage.From != nil:
+			return upd.EditedMessage.From.Username, true
+		case upd.ChannelPost != nil && upd.ChannelPost.From != nil:
+			return upd.ChannelPost.From.Username, true
+		case upd.EditedChannelPost != nil && upd.EditedChannelPost.From != nil:
+			return upd.EditedChannelPost.From.Username, true
+		case upd.BusinessConnection != nil:
+			return upd.BusinessConnection.User.Username, true
+		case upd.BusinessMessage != nil && upd.BusinessMessage.From != nil:
+			return upd.BusinessMessage.From.Username, true
+		case upd.EditedBusinessMessage != nil && upd.EditedBusinessMessage.From != nil:
+			return upd.EditedBusinessMessage.From.Username, true
+		case upd.MessageReaction != nil && upd.MessageReaction.User != nil:
+			return upd.MessageReaction.User.Username, true
+		case upd.InlineQuery != nil && upd.InlineQuery.From != nil:
+			return upd.InlineQuery.From.Username, true
+		case upd.ChosenInlineResult != nil:
+			return upd.ChosenInlineResult.From.Username, true
+		case upd.CallbackQuery != nil:
+			return upd.CallbackQuery.From.Username, true
+		case upd.ShippingQuery != nil && upd.ShippingQuery.From != nil:
+			return upd.ShippingQuery.From.Username, true
+		case upd.PreCheckoutQuery != nil && upd.PreCheckoutQuery.From != nil:
+			return upd.PreCheckoutQuery.From.Username, true
+		case upd.PurchasedPaidMedia != nil:
+			return upd.PurchasedPaidMedia.From.Username, true
+		case upd.PollAnswer != nil && upd.PollAnswer.User != nil:
+			return upd.PollAnswer.User.Username, true
+		case upd.MyChatMember != nil:
+			return upd.MyChatMember.From.Username, true
+		case upd.ChatMember != nil:
+			return upd.ChatMember.From.Username, true
+		case upd.ChatJoinRequest != nil:
+			return upd.ChatJoinRequest.From.Username, true
+		}
+	}
+
+	return "", false
+}
